bb2779820bbe189275b2fe64538dff285db2cb68
pxai <pello_altadill@cuatrovientos.org>
2017-07-01 19:19:16 +0200

    Almost there, friends

--- src/routes/post.js
+++ src/routes/post.js
@@ -4,6 +4,8 @@
  * Pello Altadill - http://pello.io
  */
 var http = require('http');
+const urlParser = require('../parser/urlParser');
+
 
 module.exports = function (app) {
 
@@ -11,12 +13,12 @@ module.exports = function (app) {
      * request
      */
     app.post('/m', function(req, res) {
-        var url = req.body.u;
+        const url = new urlParser.UrlParser(req.body.u);
         var response_msg = '';
 
-        response_msg += 'TODO, make something about this ' + url;
-        options = {  host: 'www.google.es',
-                     path: '/'};
+        //response_msg += 'TODO, make something about this ' + url.getHostname();
+        options = {  host: url.getHostname(),
+                     path: url.getPathname()};
 
         var http_req = http.request(options, function(remote_response) {
             console.log('STATUS: ' + remote_response.statusCode);
@@ -32,7 +34,7 @@ module.exports = function (app) {
             remote_response.on('end', function (err) {
                 console.log('ENDED: ');
                 //if (err) throw err;
-                response_msg += 'Yeah, let\'s build it: ' + data;
+                response_msg += data;
                 res.send(response_msg);
             });
         });
