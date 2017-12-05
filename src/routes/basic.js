c36217904077bdf201c574b28c0070c0778941a4
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-06 00:03:15 +0100

    changed pages

--- src/routes/basic.js
+++ src/routes/basic.js
@@ -3,12 +3,12 @@ module.exports = function (app) {
 
     // home
     app.get('/h', function(req, res) {
-         res.send('I will never help');
+         res.send('Check this out: https://github.com/pxai/wzw.io');
     });
 
     // about
     app.get('/a', function(req, res) {
-        res.send('<a href="http://pello.io">pello.io</a>');
+        res.send('<a href="http://pello.io">Pello Xabier Altadill Izura - pello.io</a>');
     });
 
 }
