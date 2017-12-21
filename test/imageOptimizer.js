15408c0a366d052df249fb52a990ba25c9264b32
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-12-22 00:02:59 +0100

    added failing test

---
+++ test/imageOptimizer.js
@@ -0,0 +1,18 @@
+
+const assert = require('assert');
+const cssOptimizer = require('../src/optimizer/imageOptimizer');
+const rootUrl = 'http://wzw.io/p/';
+const optimizer = new imageOptimizer.ImageOptimizer();
+
+beforeEach(function() {
+
+});
+
+
+describe('#imageOptimizer.toString()', function() {
+  it('responds to String with one message', function() {
+      assert.equal(optimizer.toString(),'Name: ImageOptimizer');
+  });
+});
+
+/
