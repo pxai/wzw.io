51b7d19b96a8b80a7d31db58952614ad3fef84a0
Pello Altadill <pello_altadill@cuatrovientos.org>
2018-04-23 23:48:57 +0200

    Corrected tests

--- test/htmlOptimizer.js
+++ test/htmlOptimizer.js
@@ -11,7 +11,7 @@ const sampleHTML = '<script type=\"text\/javascript\">var abc= \"hello\";<\/scri
             '<\/div>\r\n        <\/div>\r\n      <\/div>\r\n    <\/div>\r\n  <\/div>\r\n<\/div>' +
             '<pre>       xxxx<\/pre><span>x<\/span> <span>Hello<\/span> <b>billy<\/b>     \r\n' +
             '<input type=\"text\">\r\n<textarea><\/textarea>\r\n<pre><\/pre>';
-const optimizedHTML = '<script>var abc="hello"</script><style>#foo{color:red}</style><div><div><div><div><!--! hello --><div><div><textarea disabled>     this is a textarea </textarea></div></div></div></div></div></div><pre>       xxxx</pre><span>x</span> <span>Hello</span> <b>billy</b> <input><textarea></textarea><pre></pre>';
+const optimizedHTML = '<script>var abc="hello"</script><style>#foo{color:red}</style><div><div><div><div><!--! hello --><div><div><textarea disabled>     this is a textarea </textarea></div></div></div></div></div></div><pre>       xxxx</pre><span>x</span> <span>Hello</span> <b>billy</b> <input><textarea></textarea><pre></pre><div style=\'background-color: orange;position:absolute;top:0;right:0;\'><b>HTML</b>: 305 reduced to 305 57%</div>';
 
 beforeEach(function() {
 });
@@ -36,7 +36,7 @@ describe('#htmlOptimizer.optimize()', function() {
  });
 
    it('responds with optimized html with some html', function() {
-    return expect(optimizer.optimize('<div><p>\n\r\r\r</p></div>')).to.eventually.equal('<div><p></div>');
+    return expect(optimizer.optimize('<div><p>\n\r\r\r</p></div>')).to.eventually.equal('<div><p></div><div style=\'background-color: orange;position:absolute;top:0;right:0;\'><b>HTML</b>: 14 reduced to 14 64%</div>');
    });
 
    it('responds with optimized html with complex html', function() {
